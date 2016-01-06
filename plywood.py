9d2c64677f74cdc5aeccb818e382fdb3cfbea2ee
Kent Gustavsson <kent@minoris.se>
2016-01-06 14:57:41 +0100

    Add overcut with fixed radius
    Signed-off-by: Kent Gustavsson <kent@minoris.se>

--- plywood.py
+++ plywood.py
@@ -9,6 +9,7 @@ class Join:
 		self.cuts = cuts
 		self.edge = edge
 		self.side = side
+		self.radius = 2
 
 	def cutters(self, edge):
 		if ( self.side%2 == 0 ):
@@ -20,18 +21,27 @@ class Join:
 		margin = 0.03
 		jump = 0
 		step = (length - self.offset) / (self.cuts*2.0-1)
+		radius = self.radius + margin
 
 		cutters = union()
 		for i in range(self.cuts-edge):
 			if ( edge == 0 ):
 				jump = i
 				cutters.add(translate([jump*(step-self.offset) + i * (step+self.offset) - margin, -0.005, -0.005]) (
-					cube([step+margin*2 + self.offset, self.cutter.materialsize + 0.01, self.cutter.materialsize+0.01]))
+					cube([step+margin*2 + self.offset, self.cutter.materialsize + 0.01, self.cutter.materialsize+0.01]),
+					translate([0, 0, -radius/2])(cube([radius, self.cutter.materialsize+0.01,radius])),
+					translate([0, 0, self.cutter.materialsize-radius/2])(cube([radius, self.cutter.materialsize+0.01,radius])),
+					translate([step+margin*2 + self.offset - radius, 0, -radius/2])(cube([radius, self.cutter.materialsize+0.01,radius])),
+					translate([step+margin*2 + self.offset - radius, 0, self.cutter.materialsize-radius/2])(cube([radius, self.cutter.materialsize+0.01,radius])))
 				)
 			else:
 				jump = i+1
 				cutters.add(translate([jump*(step+self.offset) + i * (step-self.offset) - margin, -0.005, -0.005]) (
-					cube([step+margin*2 - self.offset, self.cutter.materialsize+0.01, self.cutter.materialsize+0.01]))
+					cube([step+margin*2 - self.offset, self.cutter.materialsize+0.01, self.cutter.materialsize+0.01]),
+					translate([0, -radius/2, 0])(cube([radius, radius, self.cutter.materialsize+0.01])),
+					translate([0, self.cutter.materialsize-radius/2,0])(cube([radius, radius, self.cutter.materialsize+0.01])),
+					translate([step+margin*2 + self.offset - radius, -radius/2, 0])(cube([radius, radius, self.cutter.materialsize+0.01])),
+					translate([step+margin*2 + self.offset - radius, self.cutter.materialsize-radius/2, 0])(cube([radius, radius, self.cutter.materialsize+0.01])))
 				)
 
 		return cutters
