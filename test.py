fef408a6d4300e8ce1e0c7b5cd71ccbee5f68c03
Kent Gustavsson <kent@mac.wr25.org>
2016-01-16 15:54:08 +0000

    Add boxsize calculation test
    Signed-off-by: Kent Gustavsson <kent@mac.wr25.org>

--- test.py
+++ test.py
@@ -1,11 +1,15 @@
 from plywood import Plywood,Join
 from solid import *
 
-p = Plywood(60,40)
-p2 = Plywood(60,30)
-p3 = Plywood(60,30)
-p4 = Plywood(30,40.0001)
-p5 = Plywood(30,40.0001)
+boxlength = 80
+boxwidth = 60
+boxheight = 20
+
+p = Plywood(boxlength,boxwidth)
+p2 = Plywood(boxlength,boxheight)
+p3 = Plywood(boxlength,boxheight)
+p4 = Plywood(boxheight,boxwidth+.0001)
+p5 = Plywood(boxheight,boxwidth+.0001)
 p.addCutSides(Join(p2, p, 2))
 p.addCutSides(Join(p3, p, 2))
 p.addCutSides(Join(p4, p, 1))
@@ -21,10 +25,10 @@ parts += [p,p2,p3,p4,p5]
 scene = union()
 
 scene += p.getSolid()
-scene += color([1,0,0])(translate([0,40/2+0.001,30/2 - 0.001])(rotate(v=[1,0,0], a=90)(p2.getSolid())))
-scene += color([0,1,0])(translate([0,-40/2-0.001+p3.materialsize,30/2 - 0.001])(rotate(v=[1,0,0], a=90)(p3.getSolid())))
-scene += color([0,0,1])(translate([15,0,p4.width/2-0.001])(rotate(v=[0,1,0], a=90)(p4.getSolid())))
-scene += color([0,0,1])(translate([-15,0,p4.width/2-0.001])(rotate(v=[0,1,0], a=90)(p5.getSolid())))
+scene += color([1,0,0])(translate([0,boxwidth/2+0.001,boxheight/2 - 0.001])(rotate(v=[1,0,0], a=90)(p2.getSolid())))
+scene += color([0,1,0])(translate([0,-boxwidth/2-0.001+p3.materialsize,boxheight/2 - 0.001])(rotate(v=[1,0,0], a=90)(p3.getSolid())))
+scene += color([0,0,1])(translate([boxlength/2 - p4.materialsize,0,p4.width/2-0.001])(rotate(v=[0,1,0], a=90)(p4.getSolid())))
+scene += color([0,0,1])(translate([-boxlength/2,0,p4.width/2-0.001])(rotate(v=[0,1,0], a=90)(p5.getSolid())))
 
 
 for part in parts:
